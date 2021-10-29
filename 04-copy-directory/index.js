0b1255dddd21358a21e3e848b991e4a4047a9ed4
ShikiLe <mariajane1897@gmail.com>
2021-10-30 00:35:59 +0300

    feat: add solution for task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs').promises;
+const path = require('path');
 
+fs.mkdir('./04-copy-directory/files-copy',{recursive: true})
+.then(() => {
+    return
+})
+
+fs.readdir('./04-copy-directory/files', { encoding: "utf-8", withFileTypes: true })
+.then((filenames) => {
+    for (let filename of filenames) {
+        fs.writeFile(
+            path.join('./04-copy-directory/files-copy', `${filename.name}`),
+            '',
+            (err) => {
+              if (err) throw err;
+            });
+      fs.copyFile(`./04-copy-directory/files/${filename.name}`, `./04-copy-directory/files-copy/${filename.name}`)
+      }
+    })
+  .catch((err) => {
+    console.log(err);
+  });
