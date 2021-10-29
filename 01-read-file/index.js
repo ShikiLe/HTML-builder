79656458016a21dfd204cdcaed8ce60351231d5b
ShikiLe <mariajane1897@gmail.com>
2021-10-29 13:45:17 +0300

    refactor: delete unused code

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,4 @@
 const fs = require('fs');
-const path = require('path');
-
-path.resolve('/d/RSSchool/HTML-builder/01-read-file/index.js');
 
 let stream = fs.createReadStream('./01-read-file/text.txt');
 
