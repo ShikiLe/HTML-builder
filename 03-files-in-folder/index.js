e4b5efd3f75fcbc427ccbbae783bb65678f9563f
ShikiLe <mariajane1897@gmail.com>
2021-11-02 17:23:54 +0300

    fix: fix task 3 solution

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,16 +1,14 @@
-const fs = require("fs").promises;
-const path = require("path");
-const dirPath = "/RSSchool/HTML-builder/03-files-in-folder/secret-folder";
+import fs from 'fs/promises';
+import path from 'path';
+const dirPath = '/RSSchool/HTML-builder/03-files-in-folder/secret-folder';
 
-fs.readdir(dirPath, { encoding: "utf-8", withFileTypes: true })
+fs.readdir(dirPath, { encoding: 'utf-8', withFileTypes: true })
   .then((filenames) => {
     for (let filename of filenames) {
       if (filename.isFile()) {
-        const stat = fs.stat(`${dirPath}/${filename.name}`)
+        fs.stat(`${dirPath}/${filename.name}`)
           .then((stats) => {
-            console.log(`${filename.name.split(".")[0]} - 
-                        ${path.extname(filename.name).slice(1)} - 
-                        ${stats.size / 1000}kb`
+            console.log(`${filename.name.split('.')[0]} - ${path.extname(filename.name).slice(1)} - ${stats.size / 1000}kb`
             );
           })
           .catch((error) => {
