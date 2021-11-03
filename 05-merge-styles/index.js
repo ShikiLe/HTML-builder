4e11d30e986ba11c58c9d0db5723e85f89780321
ShikiLe <mariajane1897@gmail.com>
2021-11-03 15:32:17 +0300

    feat: add task 5 solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,32 @@
+import fs from 'fs/promises';
+import path from 'path';
+const dirPath = '/RSSchool/HTML-builder/05-merge-styles/styles';
+let stylesStr = '';
+
+
+
+async function copyStyles () {
+  fs.writeFile(path.join('05-merge-styles', 'project-dist', 'bundle.css'), '', (err) => {
+    if (err) throw err;
+  });
+  try {
+    const files = await fs.readdir(dirPath, { encoding: 'utf-8', withFileTypes: true });
+
+    for (let file of files) {
+
+      if (file.isFile() && `${path.extname(file.name).slice(1)}` === 'css') {   
+        const currFile =  await fs.readFile(path.join('05-merge-styles', 'styles', `${file.name}`), 'utf-8');
+        stylesStr += currFile;
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+  await fs.appendFile(path.join('05-merge-styles', 'project-dist', 'bundle.css'), stylesStr,
+    err => {
+      if (err) throw err;
+    });
+}
+
+copyStyles();
+
