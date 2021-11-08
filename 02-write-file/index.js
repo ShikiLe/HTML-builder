2625ad562920bc873fd7110705e3ad1235b2262e
ShikiLe <mariajane1897@gmail.com>
2021-11-08 17:32:11 +0300

    refactor:refactor task 2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -4,30 +4,19 @@ import path from 'path';
 const stdout = process.stdout;
 const stdin = process.stdin;
 
-async function isFileExist (file) {
-  try {
-    await fs.access(file);
-    return true;
-  } catch (err) {
-    return false;
-  }
-}
-
 stdout.write('Please, enter your message\n');
+await fs.writeFile(
+      path.join('02-write-file/', 'text.txt'),
+      '');
 
 stdin.on('data', async (data) => {
   if (data.includes('exit')) {
     process.exit(); 
   }
-  if (await !isFileExist('02-write-file/text.txt')) {
-    await fs.writeFile(
+  await fs.appendFile(
       path.join('02-write-file/', 'text.txt'),
       data);
-  } else {
-    await fs.appendFile(
-      path.join('02-write-file/', 'text.txt'),
-      data);
-  }
+
 });
 
 process.on('SIGINT', function() {
