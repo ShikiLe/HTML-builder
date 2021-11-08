8bccb65a06a85bd6c5d37a9e640911a9e979b7ff
ShikiLe <mariajane1897@gmail.com>
2021-11-08 17:56:11 +0300

    fix: fix task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -18,12 +18,37 @@ async function copyDir () {
           });
         fs.copyFile(`./04-copy-directory/files/${filename.name}`, `./04-copy-directory/files-copy/${filename.name}`);
       }
+    actualizeDir();
     })
     .catch((err) => {
       console.log(err);
     }); 
+    
+}
+
+async function isFileExist (file) {
+  try {
+    await fs.access(file);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+async function actualizeDir () {
+   try {
+    const files = await fs.readdir('./04-copy-directory/files-copy/', { encoding: 'utf-8', withFileTypes: true, recursive: true });
+    for (let file of files) {
+      let actualFile = await isFileExist(`./04-copy-directory/files/${file.name}`);
+      let fileCopy = await isFileExist(`./04-copy-directory/files-copy/${file.name}`);
+      if (!actualFile && fileCopy) {
+        console.log('works')
+          fs.unlink(`./04-copy-directory/files-copy/${file.name}`);
+        }
+    }
+  } catch (err) {
+    console.error(err)
+  }       
 }
 
 copyDir();
-
-
